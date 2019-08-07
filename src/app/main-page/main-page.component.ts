a6ba6132d111ba1e7e2d02a9a38bced6a7bd7e7c
Konimiks <dyachenko.dmtr@gmail.com>
2019-08-07 20:45:45 +0300

    added 1 heal

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -79,6 +79,7 @@ export class MainPageComponent implements OnInit {
       'Альбедааое',
       'Арночка',
       'Аррсенал',
+      'Антерос',
       'Бабушказачем',
       'Бэдтрик',
       'Везульвий',
